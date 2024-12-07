65abd63f7adf25d38f084c5bf93039f441f7c9df
Colin Chong Kai ZHe <131168288+ColinZhe-1213@users.noreply.github.com>
2024-12-07 15:47:09 +0000

    Patch 5 (#15)
    * Modify OTPgeneration with include of store sata in
    to Json file as log
    
    * try fetching file location

---
+++ PROTOTYPE/testfile/Test_OTPgeneration.py
@@ -0,0 +1,12 @@
+import sys 
+import os
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '../code'))
+try:
+    from OTPgeneration import OTPgeneration
+except ImportError:
+    print("Module OTPgeneration not found. Please ensure it is correctly named and located in the '../code' directory.")
+    exit(1) 
+
+
+    
