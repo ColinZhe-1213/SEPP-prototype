04a7a0a48f115e0ae39dc676f433de361c3b8ab8
ColinZhe-1213 <cckz@hotmail.my>
2024-12-07 14:41:00 +0000

    Modify OTPgeneration to allows data being save into JSON file as log

--- PROTOTYPE/code/OTPgeneration.py
+++ PROTOTYPE/code/OTPgeneration.py
@@ -1,13 +1,20 @@
 import pyotp
 from cryptography.fernet import Fernet
+import json
+import atexit 
+import os
 
 class OTPgeneration:
     # Initialize
-    def __init__(self):
-        self.users={}
+    def __init__(self,data_directory="code/data", json_filename="userdata.json"):
         self.key = Fernet.generate_key()
         self.fernet = Fernet(self.key)
-    
+        self.data_directory = data_directory
+        self.json_filename = json_filename
+        os.makedirs(self.data_directory, exist_ok=True)
+        self.users = self.load_users_from_file()
+        atexit.register(self.reset_json_data)
+        
     #Generate random OTP secret
     def generate_OTPsecret(self):
         return pyotp.random_base32()
@@ -30,8 +37,8 @@ class OTPgeneration:
             secret = self.generate_OTPsecret()
             encrypted_secret = self.fernet.encrypt(secret.encode())
             self.users[username] = {
-                'secret': encrypted_secret,
-                'counter': 1 
+                "secret": encrypted_secret,
+                "counter": 1 
             }
             print("User " + username + " added successfully.")
     
@@ -50,14 +57,33 @@ class OTPgeneration:
             return None
         else:
             user = self.users[username]
-            encrypted_secret = user['secret']
-            counter = user['counter']
+            encrypted_secret = user["secret"]
+            counter = user["counter"]
             secret = self.decrypt_OTPsecret(encrypted_secret)
             hotp = pyotp.HOTP(secret)
             otp = hotp.at(counter)
-            self.users[username]['counter'] += 1
+            self.users[username]["counter"] += 1
             return otp
-        
-        
+    
+    #Save the user data into JSON
+    def save_user_data(self):
+        with open(self.data_file, "w") as f:
+            json.dump(self.users, f, indent=4)
+    
+    #Load the user data from JSON
+    def load_user_data(self):
+        if os.path.exists(self.data_file):
+            with open(self.get_file_path(), "r") as file:
+                return json.load(file)
+        else:
+            print("FileNotFoundError")
+            return {}
+    
+    #Display user data log
+    def display_log(self):
+        for username, data in self.users.items():
+             print("User: " + username + ", Counter: " + str(data["counter"]) + ", Secret (Encrypted): " + data["secret"])
+
+
 
  
