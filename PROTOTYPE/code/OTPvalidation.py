fe5fec6886297279a503e7cee6bfaa90cc937fd1
kengxiang <kengxiangtan@gmail.com>
2024-12-07 17:39:30 +0000

    update OTPvalidation

--- PROTOTYPE/code/OTPvalidation.py
+++ PROTOTYPE/code/OTPvalidation.py
@@ -1,1 +1,25 @@
 import pyotp
+
+class OTPgeneration:
+
+    def OTP_validation(self,username,input_otp):
+    # validate the OTP
+    #username
+        if username not in self.users:
+            print(f"User {username} not found")
+            return False
+    
+        user = self.users[username]
+        encrypted_secret = user["secret"]
+        counter = user["counter"]-1
+        secret = self.decrypt_OTPsecret(encrypted_secret)
+
+        hotp = pyotp.HOTP(secret)
+        if hotp.verify(input_otp, counter):
+            print(f"{username} succeessfully validate the OTP .")
+            return True
+        else:
+            print(f"{username} is failed to validate the OTP.")
+            return False
+    
+    """if we want to check the OTP has expired or not"""
