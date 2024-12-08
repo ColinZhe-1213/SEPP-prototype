201676806182b96e9dd51cefb2c455e28d1f165e
kengxiang <116014269+kengxiang@users.noreply.github.com>
2024-12-08 19:15:07 +0000

    Patch 24 (#38)
    * Modify OTPgeneration with include of store sata in
    to Json file as log
    
    * try fetching file location
    
    * create testfile for OTPgeneration
    
    * update test file
    
    * update typo in testfile for Test_OTPgeneration
    
    * edit Makefile
    
    * Merge branch 'main' of https://github.com/ColinZhe-1213/SEPP-prototype into Patch-6
    
    * update Test_OTPgeneration
    
    * delete cache file
    
    * obtain previous code file
    
    * FIX OTPgeneration
    
    * Remove JSON temporary for testing
    
    * Merge branch 'main' of https://github.com/ColinZhe-1213/SEPP-prototype into Patch11
    
    * Fix OTPgeneration: Now all fucntions can be used
    
    * Merge branch 'main' of https://github.com/ColinZhe-1213/SEPP-prototype into Patch-12
    
    * Fix merge issue
    
    * remove duplciates
    
    * Merge branch 'main' of https://github.com/ColinZhe-1213/SEPP-prototype into Patch-13
    
    * Update work status
    
    * patch 18
    
    * update class
    
    * modified:   PROTOTYPE/code/OTPvalidation.py
    modified:   PROTOTYPE/code/__pycache__/OTPgeneration.cpython-312.pyc
    new file:   PROTOTYPE/code/__pycache__/OTPvalidation.cpython-312.pyc
    new file:   PROTOTYPE/code/data/userdata.json
    	modified:   PROTOTYPE/testfile/Test_OTPvalidation.py
    Changes not staged for commit:
    modified:   PROTOTYPE/code/OTPvalidation.py
    
    * modified:   PROTOTYPE/code/OTPvalidation.py
    
    * upload again OTPvalidation
    
    * patch 22
    
    * upload OTPvalidation
    
    * patch 24
    
    ---------
    
    Co-authored-by: ColinZhe-1213 <cckz@hotmail.my>

--- PROTOTYPE/prototypecode/OTPvalidation.py
+++ PROTOTYPE/prototypecode/OTPvalidation.py
@@ -1,10 +1,11 @@
 import pyotp
 from OTPgeneration import OTPgeneration
+
 class OTPvalidation:
 
     #initialize
-    def __init__(self, otp_generation_instance):
-        self.otp_gen = otp_generation_instance
+    def __init__(self, otp_gen):
+        self.otp_gen = otp_gen
 
     def OTP_validation(self,input_otp):
     # validate the OTP format
@@ -19,15 +20,18 @@ class OTPvalidation:
         
         for username,user in self.otp_gen.users.items():
             encrypted_secret = user["secret"]
-            counter = user["counter"]
-            secret = self.decrypt_OTPsecret(encrypted_secret)
+            counter_before_increment = user["counter"] - 1
+            secret = self.otp_gen.decrypt_OTPsecret(encrypted_secret)
             hotp = pyotp.HOTP(secret)
+            generated_otp = hotp.at(counter_before_increment)
 
-            if hotp.verify(input_otp, counter):
-                self.otp_gen.users[username]["counter"] += 1
-                print(f"{username} succeessfully validate the OTP .")
+            if input_otp == generated_otp:
+                print("OTP is valid, smart door unlock")
                 return True
-            else:
-                print(f"{username} is failed to validate the OTP.")
-                return False
+            
+        print("OTP is unvalid ,OTP not found or already used, smart door locked")
+        return False
+
+
+
     """if we want to check the OTP has expired or not"""
