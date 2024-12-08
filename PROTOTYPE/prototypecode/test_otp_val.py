923d988700a48267ca4c1b6c762e60e4b9c4c92b
kengxiang <116014269+kengxiang@users.noreply.github.com>
2024-12-08 20:51:08 +0000

    Patch26 (#40)
    * Modify OTPgeneration with include of store sata in
    to Json file as log
    
    * try fetching file location
    
    * create testfile for OTPgeneration
    
    * update test file
    
    * update typo in testfile for Test_OTPgeneration
    
    * edit Makefile
    
    * Merge branch 'main' of https://github.com/ColinZhe-1213/SEPP-prototype into Patch-6
    
    * update Test_OTPgeneration
    
    * delete cache file
    
    * obtain previous code file
    
    * FIX OTPgeneration
    
    * Remove JSON temporary for testing
    
    * Merge branch 'main' of https://github.com/ColinZhe-1213/SEPP-prototype into Patch11
    
    * Fix OTPgeneration: Now all fucntions can be used
    
    * Merge branch 'main' of https://github.com/ColinZhe-1213/SEPP-prototype into Patch-12
    
    * Fix merge issue
    
    * remove duplciates
    
    * Merge branch 'main' of https://github.com/ColinZhe-1213/SEPP-prototype into Patch-13
    
    * Update work status
    
    * patch 18
    
    * update class
    
    * modified:   PROTOTYPE/code/OTPvalidation.py
    modified:   PROTOTYPE/code/__pycache__/OTPgeneration.cpython-312.pyc
    new file:   PROTOTYPE/code/__pycache__/OTPvalidation.cpython-312.pyc
    new file:   PROTOTYPE/code/data/userdata.json
    	modified:   PROTOTYPE/testfile/Test_OTPvalidation.py
    Changes not staged for commit:
    modified:   PROTOTYPE/code/OTPvalidation.py
    
    * modified:   PROTOTYPE/code/OTPvalidation.py
    
    * upload again OTPvalidation
    
    * patch 22
    
    * upload OTPvalidation
    
    * patch 24
    
    * patch25
    
    * modified:   PROTOTYPE/prototypecode/data/userdata.json
    
    * patch26
    
    ---------
    
    Co-authored-by: ColinZhe-1213 <cckz@hotmail.my>

--- PROTOTYPE/prototypecode/test_otp_val.py
+++ PROTOTYPE/prototypecode/test_otp_val.py
@@ -1,32 +1,46 @@
+import pytest
 from OTPgeneration import OTPgeneration
 from OTPvalidation import OTPvalidation
 
-def test_otp_validation_terminal():
+def test_otp_validation():
     otp_gen = OTPgeneration()
-    otp_gen.add_user("user1")
-    otp_gen.add_user("user2")
+    otp_gen.add_user("user 1")
+    otp_gen.add_user("user 2")
     
-    otp1_user1 = otp_gen.generateOTP("user1")
-    otp1_user2 = otp_gen.generateOTP("user2")
-    print(f"\nGenerated OTP for user1: {otp1_user1}")
-    print(f"Generated OTP for user2: {otp1_user2}")
+    otp1_user1 = otp_gen.generateOTP("user 1")
+    otp1_user2 = otp_gen.generateOTP("user 2")
+    print("OTP for user 1: " + otp1_user1)
+    print("OTP for user 2: " + otp1_user2)
     
     otp_val = OTPvalidation(otp_gen)
+    print("Testing OTP validation...")
+    
+    assert otp_val.OTP_validation(otp1_user1) is True, "Valid OTP for user1 failed"
+    print("Test case passed: OTP for user 1 is valid.")
+    
+    assert otp_val.OTP_validation(otp1_user2) is True, "Valid OTP for user2 failed"
+    print("Test case passed: OTP for user 2 is valid.")
+    
+    invalid_otp = "123456"
+    assert otp_val.OTP_validation(invalid_otp) is False, "Invalid OTP should fail"
+    print("Test case passed: Invalid OTP fails.")
 
-    print("\n--- Start testing OTP validation ---")
-    input_otp_user1 = input("\nEnter OTP for user1: ")
-    otp_val.OTP_validation(input_otp_user1)
+    otp2_user1 = otp_gen.generateOTP("user 1")
 
-    input_otp_user2 = input("\nEnter OTP for user2: ")
-    otp_val.OTP_validation(input_otp_user2)
+    assert otp_val.OTP_validation(otp1_user1) is False, "Expired OTP for user 1 should fail"
+    print("Test case passed: OTP for user1 is expired.")
 
-    invalid_otp = input("\nEnter an invalid OTP to test: ")
-    otp_val.OTP_validation(invalid_otp)
+    assert otp_val.OTP_validation(otp2_user1) is True, "Second OTP for user 1 should be valid"
+    print("Test case passed: OTP for user 1 is valid after regeneration.")
 
-    otp2_user1 = otp_gen.generateOTP("user1")
-    print(f"\nGenerated next OTP for user1: {otp2_user1}")
-    input_otp_user1_new = input("\nEnter next OTP for user1 (after counter increment): ")
-    otp_val.OTP_validation(input_otp_user1_new)
+    lessthan6digitOTP = "1233"
+    assert otp_val.OTP_validation(lessthan6digitOTP) is False, "Less than 6 digits OTP should fail"
+    print("Test case passed: OTP is less than 6 digits.")
 
-if __name__ == "__main__":
-    test_otp_validation_terminal()
+    morethan6digitOTP = "1234567"
+    assert otp_val.OTP_validation(morethan6digitOTP) is False, "More than 6 digits OTP should fail"
+    print("Test case passed: OTP is more than 6 digits.")
+    
+    non_numeric_otp = "ABC123"
+    assert otp_val.OTP_validation(non_numeric_otp) is False, "OTP should only contain digits"
+    print("Test case passed: OTP with non-numeric characters fails.")
